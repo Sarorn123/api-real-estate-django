f7f375d8e5d304fa7000e4f01fdf70ef21f311f3
rorn <rysarorn8@gmail.com>
2021-10-23 00:40:03 +0700

    nerr

--- API-REAL-ESTATE/settings.py
+++ API-REAL-ESTATE/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7#f&n&=b$4q!6(716)9%)9vh0j57w6t!%wioagm57b811@0ko@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '']
+ALLOWED_HOSTS = ['127.0.0.1', 'django-api-real-estate.herokuapp.com']
 
 
 # Application definition
